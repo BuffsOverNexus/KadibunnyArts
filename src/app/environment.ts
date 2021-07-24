9b9192dcac0bccdd1f73870e5e333ea410109926
rgoldbe4 <rgoldbe4@uncc.edu>
2021-07-24 16:59:52 -0400

    Issue #21 - Ensuring URLs go to the right place.

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,4 +1,17 @@
+import { isDevMode } from '@angular/core';
+
 export enum Environment {
   LOCAL_URL = 'http://127.0.0.1:5000/v1/',
-  PRODUCTION_URL = 'https://kadibunnyart-api.herokuapp.com/v1/'
+  PRODUCTION_URL = 'https://kadibunnyart-api.herokuapp.com/v1/',
 }
+
+export class ENV {
+  public static getEnvironment(): Environment {
+    if (isDevMode()) {
+      return Environment.LOCAL_URL;
+    } else {
+      return Environment.PRODUCTION_URL;
+    }
+  }
+}
+
